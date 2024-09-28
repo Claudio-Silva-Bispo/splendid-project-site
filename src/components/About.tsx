7a0a924d1ff5da490df0624cc150ab74545ccf70
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-09-28 08:24:55 -0300

    Inserir fotos e videos
    Former-commit-id: d7d4716aea625472451a71d653149b3099ceab4b

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import imagem from '../../public/about-company/imagem-sobre-empresa-removebg-preview.png';
+import imagem from '../../public/assets/about-company/imagem-sobre-empresa-removebg-preview.png';
 import VideoComponent from "./VideoComponent";
 
 
@@ -128,17 +128,14 @@ export default function About() {
             </div>
 
             <div className="flex gap-4 animate-marquee">
-                <img className="h-48 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/cozinha/20200123_150607.jpg" alt="Room photo" />
-                <img className="h-48 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/cozinha/20210201_131604.jpg" alt="Image 2" />
-                <img className="h-48 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/cozinha/20200701_143913.jpg" alt="Image 3" />
-                <img className="h-48 aspect-video rounded-sm object-cover object-center bg-gray-500" src="/assets/Service/cozinha/20200701_162936.jpg" alt="Image 4" />
-                <img className="h-48 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/cozinha/20201125_151653.jpg" alt="Image 5" />
-                <img className="h-48 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/sala/20201217_095105.jpg" alt="Image 6" />
-                <img className="h-48 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/sala/IMG-20200204-WA0028.jpg" alt="Image 7" />
-                <img className="h-48 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/chao/20201125_152907.jpg" alt="Image 8" />
-                <img className="h-48 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/chao/20210207_103846.jpg" alt="Image 9" />
-                <img className="h-48 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/personalizado/20201217_123521.jpg" alt="Image 10" />
-
+                <img className="h-60 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/cozinha/20200123_150607.jpg" alt="Room photo" />
+                <img className="h-60 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/cozinha/20210201_131604.jpg" alt="Image 2" />
+                <img className="h-60 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/cozinha/20200701_143913.jpg" alt="Image 3" />
+                <img className="h-60 aspect-video rounded-sm object-cover object-center bg-gray-500" src="/assets/Service/cozinha/20200701_162936.jpg" alt="Image 4" />
+                <img className="h-60 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/cozinha/20201125_151653.jpg" alt="Image 5" />
+                <img className="h-60 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/sala/20201217_095105.jpg" alt="Image 6" />
+                <img className="h-60 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/sala/IMG-20200204-WA0028.jpg" alt="Image 7" />
+                <img className="h-60 aspect-video rounded-sm object-fill object-center bg-gray-500" src="/assets/Service/chao/20201125_152907.jpg" alt="Image 8" />
             </div>
       </div>
 
